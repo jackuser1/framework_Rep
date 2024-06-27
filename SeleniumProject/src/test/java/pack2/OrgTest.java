c4355ff2c59998e44d0744a7938d359b491336a5
jackuser1 <p789483@gmail.com>
2024-06-27 13:06:15 +0530

    New commit

---
+++ SeleniumProject/src/test/java/pack2/OrgTest.java
@@ -0,0 +1,17 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createorgTest() {
+		System.out.println("execute createOrgTest");
+	}
+	
+	@Test
+	public void modifyorgTest() {
+		System.out.println("execute modifyOrgTest");
+	}
+	
+}
